008d99e9078a1ef8e15b29d0b031583c3aee05a1
Adnan <adnan.chowdhury@outlook.com>
2019-08-07 16:21:02 -0500

    Added Validation via Yup
    Checks email as correct format and Password having at least 6 characters

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,15 +1,34 @@
 import React from "react";
 import { Form, Field, withFormik } from "formik";
+import * as Yup from "yup";
 
-const userForm = props => {
+const userForm = ({ errors, touched }) => {
   return (
     <div className="">
       <h1>Welcome User, Let's get you checked-in!</h1>
-      <Form>
-        <Field type="text" name="username" placeholder="Username" />
-        <Field type="email" name="email" placeholder="Email Address" />
-        <Field type="password" name="password" placeholder="Password" />
-        <Field type="checkbox" name="terms" />
+      <Form className="user-form">
+        <Field
+          type="text"
+          name="username"
+          placeholder="Username"
+          className="user"
+        />
+        {touched.email && errors.email && <p>{errors.email}</p>}
+        <Field
+          type="email"
+          name="email"
+          placeholder="Email Address"
+          className="user email"
+        />
+        {touched.password && errors.password && <p>{errors.password}</p>}
+        <Field
+          type="password"
+          name="password"
+          placeholder="Password"
+          className=" user pass"
+        />
+        Accept the Terms of Service
+        <Field type="checkbox" name="terms" className="user check" />
         <button type="submit">Submit!</button>
       </Form>
     </div>
@@ -25,6 +44,14 @@ const FormikUserForm = withFormik({
       terms: terms || false
     };
   },
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email()
+      .required(),
+    password: Yup.string()
+      .min(6)
+      .required()
+  }),
   handleSubmit(values) {
     console.log(values);
   }
