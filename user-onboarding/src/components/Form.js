529e1d9e18e43c713208920f516aeef18c9d0cd6
Adnan <adnan.chowdhury@outlook.com>
2019-08-07 16:50:12 -0500

    clear up console.logs

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -5,7 +5,6 @@ import axios from "axios";
 
 const UserForm = ({ errors, touched, status }) => {
   const [user, setUser] = useState([]);
-  console.log(user);
   useEffect(() => {
     if (status) {
       setUser([...user, status]);
